1bf5c5604125e39c0f3283392bd2ed7b701940bd
Aymeric LOPEZ <aymeric.lopez@maif.fr>
2026-08-25 09:11:33 +0200

    feat: authentification avec jwt

--- src/main/java/co/simplon/basicauth/config/SecurityConfig.java
+++ src/main/java/co/simplon/basicauth/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package co.simplon.basicauth.config;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,7 +15,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
 
 import co.simplon.basicauth.service.AuthService;
 
@@ -22,7 +31,11 @@ public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
 
-    public SecurityConfig(AuthService authServiceInjected) {
+
+    @Value("${jwt.secret")
+    private String jwtSecret;
+
+    public SecurityConfig(AuthService authServiceInjected, JwtDecoder jwtDecoderByPublicKeyValue) {
         this.userDetailsService = authServiceInjected;
     }
 
@@ -46,7 +59,21 @@ public class SecurityConfig {
                         .requestMatchers("/auth/register").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .httpBasic(Customizer.withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        var key = new SecretKeySpec(jwtSecret.getBytes(), "HmacSHA256");
+        var immutableSecret = new ImmutableSecret<>(key);
+        return new NimbusJwtEncoder(immutableSecret);
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        var originalKey = new SecretKeySpec(jwtSecret.getBytes(), "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(originalKey).build();
+    }
+
 }
--- src/main/java/co/simplon/basicauth/init/DataInitializer.java
+++ src/main/java/co/simplon/basicauth/init/DataInitializer.java
@@ -1,22 +1,36 @@
 package co.simplon.basicauth.init;
 
+import java.util.Set;
+
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import co.simplon.basicauth.entity.RoleEntity;
 import co.simplon.basicauth.entity.TodoEntity;
+import co.simplon.basicauth.entity.UserEntity;
 import co.simplon.basicauth.repository.RoleRepository;
 import co.simplon.basicauth.repository.TodoRepository;
+import co.simplon.basicauth.repository.UserRepository;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
 
     private final TodoRepository todoRepository;
     private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final SecurityBuilder securityBuilder;
 
-    public DataInitializer(TodoRepository todoRepositoryInjected, RoleRepository roleRepositoryInjected) {
+    public DataInitializer(TodoRepository todoRepositoryInjected, RoleRepository roleRepositoryInjected, UserRepository userRepositoryInjected,
+            PasswordEncoder passwordEncoderInjected, SecurityBuilder securityBuilder) {
         this.todoRepository = todoRepositoryInjected;
         this.roleRepository = roleRepositoryInjected;
+        this.userRepository = userRepositoryInjected;
+        this.passwordEncoder = passwordEncoderInjected;
+        this.securityBuilder = securityBuilder;
     }
 
     @Override
@@ -32,6 +46,19 @@ public class DataInitializer implements CommandLineRunner {
         RoleEntity roleAdmin = new RoleEntity();
         roleAdmin.setAuthority("ROLE_ADMIN");
         roleRepository.save(roleAdmin);
+
+        UserEntity admin = new UserEntity();
+        admin.setUsername("admin@example.com");
+        admin.setPassword(passwordEncoder.encode("securepassword"));
+        admin.setAuthorities(Set.of(roleAdmin));
+        userRepository.save(admin);
+
+        UserEntity user = new UserEntity();
+        user.setUsername("bastien@example.com");
+        user.setPassword("tacostacos");
+        user.setAuthorities(Set.of(roleUser));
+        userRepository.save(user);
+
     }
 
 }
